34df5aa12b00581384aaf6ec830b925a90843c73
joshua salceda <joshuasalceda0021@gmail.com>
2023-08-18 23:16:39 +0800

    worked on the student grades and activity scores

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -2,13 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Score;
 use Illuminate\Http\Request;
 
 class ScoreController extends Controller
 {
     public function store(Request $request) {
-        dd($request->all());
+        // dd($request->all());
+        try {
+            $scores = $request->input('scores');
 
-        $scores = $request->input('scores');
+            foreach ($scores as $studentId => $activityScores) {
+                foreach ($activityScores as $activityId => $score) {
+                    $existingScore = Score::where('student_id', $studentId)
+                    ->where('activity_id', $activityId)
+                    ->first();
+
+                    if ($existingScore) {
+                        // Update existing score
+                        $existingScore->score = $score;
+                        $existingScore->save();
+                    } else {
+                        // Create a new score
+                        $score = new Score([
+                            'student_id' => $studentId,
+                            'activity_id' => $activityId,
+                            'score' => $score,
+                        ]);
+                        $score->save();
+                    }
+                }
+            }
+            return redirect()->route('home')->with('success', 'Scores saved successfully');
+        } catch (\Exception $e) {
+            return redirect()->route('home')->with('error', 'Error saving scores');
+        }
+
     }
 }
